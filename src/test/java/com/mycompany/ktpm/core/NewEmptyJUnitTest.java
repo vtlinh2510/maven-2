f8b6164556250de7951ad38c0444478fb2413fb9
vtlinh2510 <vuthuylinh2525@gmail.com>
2024-08-16 00:46:29 +0700

    add project

---
+++ src/test/java/com/mycompany/ktpm/core/NewEmptyJUnitTest.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.ktpm.core;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class NewEmptyJUnitTest {
+    
+    @Test
+    public void testFactorialGivenRightArgumentReturnsWell(){
+        assertEquals(200, 200);
+    }
+    
+}
